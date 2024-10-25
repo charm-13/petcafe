f4197efdca6a4a6a3909cd8bb4ff1a23caff777c
Samiksha <samiksha.gk29@gmail.com>
2024-10-25 11:53:55 -0700

    get inventory outline

--- src/api/users.py
+++ src/api/users.py
@@ -12,10 +12,18 @@ router = APIRouter(
 )
 
 @router.post("/{user_id}/inventory")
-def get_inventory():
-    # with db.engine.begin() as connection:
-    #     connection.execute(
-    #         sqlalchemy.text()
-    #     )
-    # return "OK"
+def get_inventory(user_id: int):
+    with db.engine.begin() as connection:
+        inventory = connection.execute(
+            sqlalchemy.text("""SELECT username, gold, treat_sku, creatures 
+                            FROM users
+                            JOIN users_inventory ON users.id users_inventory.id
+                            WHERE users.id = :id"""),
+                            {"id": user_id}).mappings().fetchone()
+    return {
+        "name": inventory["username"],
+        "treats": inventory["treat_sku"],
+        "gold": inventory["gold"], 
+        "pets": inventory["creatures"]
+    }
     
