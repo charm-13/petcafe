d4ea6f85b67da44c11020e77a151c384ea82c8d0
Charlotte Maples <cgmaples13@gmail.com>
2024-11-01 10:08:31 -0700

    added cart functions

--- src/api/carts.py
+++ src/api/carts.py
@@ -24,8 +24,17 @@ def create_cart(new_cart: User) -> dict[str, int]:
     Creates a new cart for a specific user.
     """
     with db.engine.begin() as connection:
-        pass
-    # return {"cart_id": 0}
+        id = connection.execute(
+            sqlalchemy.text("""INSERT INTO carts (user_id, time_created)
+                            SELECT :user_id, CURRENT_TIMESTAMP AT TIME ZONE 'America/Los_Angeles'
+                            RETURNING id"""),
+            {"user_id": new_cart.user_id}
+        ).mappings().fetchone()
+        
+    cart_id = id["id"]
+        
+    print(f"cart id: {cart_id}, new cart for: {new_cart}")
+    return { "cart_id": cart_id }
 
 
 class CartItem(BaseModel):
@@ -37,9 +46,33 @@ def set_item_quantity(cart_id: int, item_sku: str, cart_item: CartItem):
     """ 
     Updates the quantity of a specific item in a cart.
     """
-    with db.engine.begin() as connection:
-        pass
-    # return "OK"
+    print(f"Finding cart {cart_id}...")
+    
+    try:
+        with db.engine.begin() as connection:
+            ids = connection.execute(
+                sqlalchemy.text("""SELECT id 
+                                FROM carts 
+                                WHERE id = :cart_id"""), 
+                {"cart_id": cart_id}
+            ).fetchone()
+            
+            if ids is None:
+                print(f"cart {cart_id} doesn't exist")
+                return {"success": False} 
+        
+            connection.execute(
+                sqlalchemy.text("""INSERT INTO carts_items (cart_id, item_sku, quantity) 
+                                VALUES (:id, :sku, :amt)"""), 
+                {"id": cart_id, "sku": item_sku, "amt": cart_item.quantity}
+            )
+
+        print(f"Added {cart_item.quantity} {item_sku} to cart {cart_id}")
+        return {"success": True}
+    
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return {"success": False, "error": str(e)}
 
 
 @router.post("/{cart_id}/checkout")
@@ -47,6 +80,52 @@ def checkout(cart_id: int):
     """ 
     Handles the checkout process for a specific cart.
     """
-    with db.engine.begin() as connection:
-        pass
-    # return {"total_treats_bought": 0, "total_gold_paid": 0}
+    print(f"Starting checkout for cart {cart_id}...")
+    total_bought = 0
+    total_paid = 0
+    
+    try:
+        with db.engine.begin() as connection:
+            items = connection.execute(
+                sqlalchemy.text("""SELECT carts_items.cart_id, carts_items.item_sku, carts_items.quantity, treats.price
+                                FROM carts_items 
+                                JOIN treats ON treats.sku = carts_items.item_sku
+                                WHERE cart_id = :cart_id"""), 
+                {"cart_id": cart_id}
+            ).mappings()
+            
+            if not items:
+                print(f"cart {cart_id} is empty or doesn't exist")
+                return {"error": "Cart is empty or doesn't exist"}
+            
+            purchases = []
+            for item in items:
+                sku = item["item_sku"]
+                quantity = item["quantity"]
+                price = item["price"]
+                total_bought += quantity
+                total_paid += quantity*price
+                
+                purchases.append({"sku": sku, "quantity": quantity, "price per item": price})
+                
+            connection.execute(
+                sqlalchemy.text("""WITH user AS (
+                                    SELECT user_id
+                                    FROM carts
+                                    WHERE cart_id = :cart_id
+                                    LIMIT 1
+                                )
+                                UPDATE users 
+                                SET gold = gold + :total_paid
+                                WHERE user_id = user"""),
+                {"total_paid": total_paid})
+                
+            print(f"""cart {cart_id} bought {purchases} \n 
+                  cart {cart_id} bought {total_bought} potions and paid {total_paid} gold""") 
+            
+        return {"total_treats_bought": total_bought,
+                "total_gold_paid": total_paid}
+    
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return {"error": str(e)}
