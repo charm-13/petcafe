fde475f06cc8255aa6ebb9af5c182be52c48df8c
thuang-28 <tracymhuang@outlook.com>
2024-12-03 18:36:10 -0800

    Update schema.sql to be consistent

--- schema.sql
+++ schema.sql
@@ -9,42 +9,12 @@ CREATE TABLE public.treats (
 ) tablespace pg_default;
 
 
-INSERT INTO
-    public.treats (sku, name, satiety, price)
-VALUES
-    ('CLOUD_CANDY', 'cloud candy', 15, 15),
-    ('CRUMBLY_COOKIE', 'crumbly cookie', 20, 20),
-    ('CUCUMBER_SALAD', 'cucumber salad', 30, 30),
-    ('GOGI_BERRY', 'gogi berry', 10, 10),
-    ('HONEY', 'honey', 8, 8),
-    ('KEBAB', 'kebab', 75, 75),
-    ('NABNAB_BERRY', 'nabnab berry', 15, 15),
-    ('PINAP_BERRY', 'pinap berry', 20, 20),
-    ('RAZZ_BERRY', 'razz berry', 10, 10),
-    ('SARDINE', 'sardine', 45, 45),
-    ('SEAWEED_SNACK', 'seaweed', 25, 25),
-    ('SHADOW_BREAD', 'shadow bread', 42, 42),
-    ('SHINY_BERRY', 'shiny berry', 27, 27),
-    ('SPICE_COOKIE', 'spice cookie', 38, 38),
-    ('STEAK_SKEWER', 'steak skewer', 95, 95),
-    ('TAIYAKI', 'taiyaki', 55, 55),
-    ('VANILLA_FLUFF', 'vanilla fluff', 22, 22);
-
-
 CREATE TABLE public.evolution_stages (
     stage smallint NOT NULL,
     max_happiness smallint NOT NULL,
     max_hunger smallint NOT NULL,
     CONSTRAINT evolution_stages_pkey PRIMARY KEY (stage)
 ) tablespace pg_default;
-
-
-INSERT INTO
-    public.evolution_stages (stage, max_happiness, max_hunger)
-VALUES
-    (1, 100, 100),
-    (2, 500, 500),
-    (3, 1000, 1000); 
 
 
 CREATE TABLE public.creature_types (
@@ -56,21 +26,6 @@ CREATE TABLE public.creature_types (
     CONSTRAINT creature_types_fav_treat_fkey FOREIGN KEY (fav_treat) REFERENCES treats (sku),
     CONSTRAINT creature_types_hated_treat_fkey FOREIGN KEY (hated_treat) REFERENCES treats (sku)
 ) tablespace pg_default;
-
-
-INSERT INTO
-    public.creature_types (type, fav_treat, hated_treat)
-VALUES
-    ('water_turtle', 'SARDINE', 'KEBAB'),
-    ('fire_lizard', 'SPICE_COOKIE', 'SEAWEED_SNACK'),
-    ('grass_dragon', 'CUCUMBER_SALAD', 'SPICE_COOKIE'),
-    ('sea_ghost', 'SHADOW_BREAD', 'CRUMBLY_COOKIE'),
-    ('psychic_unicorn', 'SHINY_BERRY', 'SHADOW_BREAD'),
-    ('fighter_cow', 'GOGI_BERRY', 'STEAK_SKEWER'),
-    ('silly_cat', 'TAIYAKI', 'CUCUMBER_SALAD'),
-    ('electric_sheep', 'CLOUD_CANDY', 'SARDINE'),
-    ('fluffy_fairy', 'PINAP_BERRY', 'VANILLA_FLUFF'),
-    ('flying_bug', 'VANILLA_FLUFF', 'HONEY');
 
 
 CREATE TABLE public.creatures (
@@ -85,31 +40,6 @@ CREATE TABLE public.creatures (
     CONSTRAINT creatures_stage_fkey FOREIGN KEY (stage) REFERENCES evolution_stages (stage),
     CONSTRAINT creatures_type_fkey FOREIGN KEY (type) REFERENCES creature_types (TYPE)
 ) tablespace pg_default;
-
-
-INSERT INTO
-    public.creatures (id, name, type, happiness, hunger)
-VALUES
-    (2, 'Blaze', 'fire_lizard', 100, 0),
-    (3, 'Whiskaroo', 'silly_cat', 100, 0),
-    (4, 'Rumbull', 'fighter_cow', 100, 0),
-    (5, 'Spectrip', 'sea_ghost', 100, 0),
-    (6, 'Mindara', 'psychic_unicorn', 100, 0),
-    (7, 'Zap E. Wool', 'electric_sheep', 100, 0),
-    (8, 'Flutterbop', 'flying_bug', 100, 0),
-    (9, 'Puffilia', 'fluffy_fairy', 100, 0),
-    (10, 'Thornvyne', 'grass_dragon', 100, 0),
-    (11, 'Shellwater', 'water_turtle', 100, 0),
-    (12, 'Infernyx', 'fire_lizard', 100, 0),
-    (13, 'Nibbly', 'silly_cat', 100, 0),
-    (14, 'Brawlow', 'fighter_cow', 100, 0),
-    (15, 'Haunter Glow', 'sea_ghost', 100, 0),
-    (16, 'Mystara', 'psychic_unicorn', 100, 0),
-    (17, 'Shocuff', 'electric_sheep', 100, 0),
-    (18, 'Flyka', 'flying_bug', 100, 0),
-    (19, 'Fayblossom', 'fluffy_fairy', 100, 0),
-    (20, 'Grumblevine', 'grass_dragon', 100, 0),
-    (21, 'Aquaquel', 'water_turtle', 100, 0);
 
 
 CREATE TABLE public.users (
@@ -186,3 +116,73 @@ CREATE TABLE public.purchases (
     CONSTRAINT purchases_item_sku_fkey FOREIGN KEY (item_sku) REFERENCES treats (sku) ON UPDATE CASCADE,
     CONSTRAINT purchases_user_id_fkey FOREIGN KEY (user_id) REFERENCES users (id) ON UPDATE CASCADE ON DELETE CASCADE
 ) tablespace pg_default;
+
+
+INSERT INTO treats (sku, name, satiety, price)
+VALUES
+    ('CLOUD_CANDY', 'cloud candy', 15, 15),
+    ('CRUMBLY_COOKIE', 'crumbly cookie', 20, 20),
+    ('CUCUMBER_SALAD', 'cucumber salad', 30, 30),
+    ('GOGI_BERRY', 'gogi berry', 10, 10),
+    ('HONEY', 'honey', 8, 8),
+    ('KEBAB', 'kebab', 75, 75),
+    ('NABNAB_BERRY', 'nabnab berry', 15, 15),
+    ('PINAP_BERRY', 'pinap berry', 20, 20),
+    ('RAZZ_BERRY', 'razz berry', 10, 10),
+    ('SARDINE', 'sardine', 45, 45),
+    ('SEAWEED_SNACK', 'seaweed', 25, 25),
+    ('SHADOW_BREAD', 'shadow bread', 42, 42),
+    ('SHINY_BERRY', 'shiny berry', 27, 27),
+    ('SPICE_COOKIE', 'spice cookie', 38, 38),
+    ('STEAK_SKEWER', 'steak skewer', 95, 95),
+    ('TAIYAKI', 'taiyaki', 55, 55),
+    ('VANILLA_FLUFF', 'vanilla fluff', 22, 22)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO
+    evolution_stages (stage, max_happiness, max_hunger)
+VALUES
+    (1, 100, 100),
+    (2, 500, 500),
+    (3, 1000, 1000)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO
+    creature_types (type, fav_treat, hated_treat)
+VALUES
+    ('water_turtle', 'SARDINE', 'KEBAB'),
+    ('fire_lizard', 'SPICE_COOKIE', 'SEAWEED_SNACK'),
+    ('grass_dragon', 'CUCUMBER_SALAD', 'SPICE_COOKIE'),
+    ('sea_ghost', 'SHADOW_BREAD', 'CRUMBLY_COOKIE'),
+    ('psychic_unicorn', 'SHINY_BERRY', 'SHADOW_BREAD'),
+    ('fighter_cow', 'GOGI_BERRY', 'STEAK_SKEWER'),
+    ('silly_cat', 'TAIYAKI', 'CUCUMBER_SALAD'),
+    ('electric_sheep', 'CLOUD_CANDY', 'SARDINE'),
+    ('fluffy_fairy', 'PINAP_BERRY', 'VANILLA_FLUFF'),
+    ('flying_bug', 'VANILLA_FLUFF', 'HONEY')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO
+    creatures (id, name, type, happiness, hunger)
+VALUES
+    (1, 'Aquaquel', 'water_turtle', 100, 0),
+    (2, 'Blaze', 'fire_lizard', 100, 0),
+    (3, 'Whiskaroo', 'silly_cat', 100, 0),
+    (4, 'Rumbull', 'fighter_cow', 100, 0),
+    (5, 'Spectrip', 'sea_ghost', 100, 0),
+    (6, 'Mindara', 'psychic_unicorn', 100, 0),
+    (7, 'Zap E. Wool', 'electric_sheep', 100, 0),
+    (8, 'Flutterbop', 'flying_bug', 100, 0),
+    (9, 'Puffilia', 'fluffy_fairy', 100, 0),
+    (10, 'Thornvyne', 'grass_dragon', 100, 0),
+    (11, 'Shellwater', 'water_turtle', 100, 0),
+    (12, 'Infernyx', 'fire_lizard', 100, 0),
+    (13, 'Nibbly', 'silly_cat', 100, 0),
+    (14, 'Brawlow', 'fighter_cow', 100, 0),
+    (15, 'Haunter Glow', 'sea_ghost', 100, 0),
+    (16, 'Mystara', 'psychic_unicorn', 100, 0),
+    (17, 'Shocuff', 'electric_sheep', 100, 0),
+    (18, 'Flyka', 'flying_bug', 100, 0),
+    (19, 'Fayblossom', 'fluffy_fairy', 100, 0),
+    (20, 'Grumblevine', 'grass_dragon', 100, 0)
+ON CONFLICT DO NOTHING;
